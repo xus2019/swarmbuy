0d51a6fdda22b8104c39c2958406615d9738fe6b
xushuai5 <xushuai5@xiaomi.com>
2022-04-24 16:59:24 +0800

    style: IDEA Git test

--- src/main/java/com/inotee/swarmbuy/SwarmbuyApplication.java
+++ src/main/java/com/inotee/swarmbuy/SwarmbuyApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * change test
+ */
 @MapperScan("com.inotee.swarmbuy.mapper")
 @SpringBootApplication
 public class SwarmbuyApplication /*extends SpringBootServletInitializer*/ {
